ee4d007cd69920fb0759b28af885ecc2342407a8
0byiaks <0byiaks@users.noreply.github.com>
2026-02-09 21:54:39 +0000

    Add domain_name output to ACM module

--- modules/acm/output.tf
+++ modules/acm/output.tf
@@ -2,3 +2,8 @@ output "acm_certificate_arn" {
   description = "ARN of the ACM certificate"
   value       = aws_acm_certificate.acm_certificate.arn
 }
+
+output "domain_name" {
+  description = "Domain name used for the ACM certificate"
+  value       = var.domain_name
+}
